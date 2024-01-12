cebb8c4f8c08dd91fbe03f1f338d717b44867b6f
kashi618 <82939881+kashi618@users.noreply.github.com>
2024-01-12 12:47:04 +0000

    Comments added to feelingGodBad function

--- modules.py
+++ modules.py
@@ -1,5 +1,6 @@
 import microbit as mb
 
+# Returns if user is feeling "good" or "bad"
 def feelingGoodBad():
     mb.display.scroll("hi. how are you?",80)
     mb.display.scroll(choices[0])
@@ -8,10 +9,11 @@ def feelingGoodBad():
     currentChoice = 0
     
     while True:
+        # When the logo is pressed, shows the message again
         if mb.pin_logo.is_touched():
             mb.display.scroll("hi. how are you?",80)
         
-        
+        # Cycle through options (good,bad) using button A and B
         if mb.button_a.is_pressed():
             if currentChoice == 0 :
                 mb.display.scroll(choices[0],80)
@@ -27,5 +29,9 @@ def feelingGoodBad():
                 mb.display.scroll(choices[1],80)
                 currentChoice -= 1
         
+        # Returns the choice of the user as a string. ("good","bad")
         if mb.button_a.is_pressed() and mb.button_b.is_pressed():
-            return currentChoice
+            if currentChoice == 1:
+                return "good"
+            else:
+                return "bad"
