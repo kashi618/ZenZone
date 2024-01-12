fe4018e3b19b5772d50e01190b5033673cc34de7
kashi618 <82939881+kashi618@users.noreply.github.com>
2024-01-12 10:06:46 +0000

    Modules - button A and button B functionality

--- modules.py
+++ modules.py
@@ -2,10 +2,26 @@ import microbit as mb
 
 def welcomeMSG():
     mb.display.scroll("hi. how are you?",80)
+    mb.display.scroll(choices[0])
+    
+    choices = ["Good","Bad"] # 0=Good  1=Bad
+    currentChoice = 0
     
     while True:
         if mb.pin_logo.is_touched():
             mb.display.scroll("hi. how are you?",80)
         
+        
+        if mb.button_a.is_pressed():
+            if currentChoice == 0 :
+                mb.display.scroll(choices[0],80)
+            else:
+                mb.display.scroll(choices[1],80)
+        if mb.button_b.is_pressed():
+            if currentChoice == 0 :
+                mb.display.scroll(choices[0],80)
+            else:
+                mb.display.scroll(choices[1],80)
+        
         if (mb.button_a.is_pressed()) and (mb.button_b.is_pressed()):
-            mb.display.scroll("hi, how are you?",100)
+            
