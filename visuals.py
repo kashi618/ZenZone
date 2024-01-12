79f36fc38247bad99805bfddb5ec00cf3dd7df9f
kashi618 <82939881+kashi618@users.noreply.github.com>
2024-01-12 12:57:27 +0000

    Started drawing cup

--- visuals.py
+++ visuals.py
@@ -1,5 +1,12 @@
 import microbit as mb
 
 def cup():
-    for i in range(0,5):
-        mb.display.set_pixel(0,0,9)
+    for i in range (0,5):
+        mb.display.set_pixel(0,i,9)
+        mb.display.set_pixel(4,i,9)
+    for i in range(1,4):
+        mb.display.set_pixel(i,1,3)
+        mb.display.set_pixel(i,2,3)
+        mb.display.set_pixel(i,3,3)
+        mb.display.set_pixel(i,4,9)
+
