eaaa11b064d3b70ccc85d6ba5fa8a73988ef598e
kashi618 <nialj618@gmail.com>
2024-01-13 16:16:57 +0000

    Modules - Created startup sequence

--- modules.py
+++ modules.py
@@ -1,12 +1,31 @@
 import microbit as mb
+from microbit import display
+import music
 
+def startup():
+    display.clear()
+    for i in range (0,5):
+        display.set_pixel(0,i,9)
+        display.set_pixel(4,i,9)
+    for i in range(1,4):
+        display.set_pixel(i,1,5)
+        display.set_pixel(i,2,5)
+        display.set_pixel(i,3,5)
+        display.set_pixel(i,4,9)
+    
+    music.pitch(622, 500)
+    music.pitch(311, 125)
+    music.pitch(466, 250)
+    music.pitch(415, 500)
+    music.pitch(622, 250)
+    music.pitch(466, 250)
+    
 # Returns if user is feeling "good" or "bad"
 def feelingGoodBad():
-    mb.display.scroll("hi. how are you?",80)
-    mb.display.scroll(choices[0])
-    
     choices = ["Good","Bad"] # 0=Good  1=Bad
     currentChoice = 0
+    
+    mb.display.scroll("hi. how are you?",80)
     
     while True:
         # When the logo is pressed, shows the message again
